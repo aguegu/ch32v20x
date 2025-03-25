d4f77a4a7178103b8d112fc9df17ecfafd5eb1b1
aGuegu <weihong.guan@gmail.com>
2025-03-25 16:15:42 +0800

    mesh proxy with own HAL

---
+++ extra/examples/mesh-proxy-onoff-server/HAL/include/HAL.h
@@ -0,0 +1,77 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : HAL.h
+ * Author             : WCH
+ * Version            : V1.0
+ * Date               : 2016/05/05
+ * Description        :
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+/******************************************************************************/
+#ifndef __HAL_H
+#define __HAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "config.h"
+#include "RTC.h"
+
+/* hal task Event */
+#define HAL_REG_INIT_EVENT    0x2000
+#define HAL_TEST_EVENT        0x4000
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+extern tmosTaskID halTaskID;
+
+/*********************************************************************
+ * GLOBAL FUNCTIONS
+ */
+
+/**
+ * @brief   Hardware initialization
+ */
+extern void HAL_Init(void);
+
+/**
+ * @brief   HAL processing
+ *
+ * @param   task_id - The TMOS assigned task ID.
+ * @param   events - events to process.  This is a bit map and can
+ *                   contain more than one event.
+ */
+extern tmosEvents HAL_ProcessEvent(tmosTaskID task_id, tmosEvents events);
+
+/**
+ * @brief   Initialization of the BLE library
+ */
+extern void WCHBLE_Init(void);
+
+/**
+ * @brief   Get the internal temperature sampling value.
+ *          If the ADC interrupt sampling is used,
+ *          the interrupt is temporarily shielded in this function.
+ *
+ * @return  Internal temperature sampling value.
+ */
+extern uint16_t HAL_GetInterTempValue(void);
+
+/**
+ * @brief   Internal 32K calibration
+ */
+extern void Lib_Calibration_LSI(void);
+
+/*********************************************************************
+*********************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
